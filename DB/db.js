91861531b43fb9b5bc6c7bae71be6ec9f629696e
lovegee123 <167515974+lovegee123@users.noreply.github.com>
2025-11-29 11:28:32 +0700

    4.2

--- DB/db.js
+++ DB/db.js
@@ -4,10 +4,11 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || '10.23.251.151',
-  user: process.env.DB_USER || 'Test',
-  password: process.env.DB_PASSWORD || '',
-  database: process.env.DB_NAME || 'liftcare',
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
